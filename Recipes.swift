e35c3608e9bd15c6a4210b6b0a22150ad43aaf9f
linhuaiguan <37492525+linhuaiguan@users.noreply.github.com>
2018-05-21 14:31:20 +1000

    Add files via upload

---
+++ Recipes.swift
@@ -0,0 +1,181 @@
+//
+//  Recipes.swift
+//  LotusCook
+//
+//  Created by Luochun on 2018/5/20.
+//  Copyright © 2018年 Elase. All rights reserved.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+class Recipe: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var tag: String = ""
+    @objc dynamic var energy: String = ""
+    @objc dynamic var time: String = ""
+    var ingredients: [(String, String)] = []
+    @objc dynamic var steps: String = ""
+    @objc dynamic var image: String = ""
+    
+}
+
+extension Recipe {
+    static func getAllRecipes() -> [Recipe] {
+        let filePath = Bundle.main.path(forResource: "recipes.plist", ofType: nil)!
+        
+        let infos = NSArray(contentsOfFile: filePath) as! [JSONMap]
+        var res :[Recipe] = []
+        infos.forEach({
+            let recipe = Recipe()
+            recipe.name = $0["name"] as! String
+            recipe.tag = $0["tag"] as! String
+            recipe.energy = $0["energy"] as! String
+            recipe.time = $0["time"] as! String
+            recipe.ingredients = ($0["ingredients"] as! [String]).map({
+                let a = $0.components(separatedBy: ",")
+                return (a[0], a[1])
+            })
+            recipe.steps = $0["steps"] as! String
+            recipe.image = $0["image"] as! String + ".png"
+            res.append(recipe)
+        })
+        
+        return res
+    }
+    
+    /// 关键字搜索
+    static func getAllRecipesBy(_ words: String) -> [Recipe] {
+        var items = Recipe.getAllRecipes()
+        items = items.filter({
+            $0.name.contains(words) || $0.tag == words
+        })
+        return items
+    }
+    
+}
+
+class Meals: Object {
+    @objc dynamic var email: String?
+    @objc dynamic var recipe: Recipe?
+    @objc dynamic var copies: Int = 1
+    
+    /// 主键
+    @objc dynamic var createdTime: String = ""
+    
+    override public static func primaryKey() -> String? {
+        return "createdTime"
+    }
+    
+    
+}
+
+func == (lhs: Meals, rhs: Meals) -> Bool
+{
+    return lhs.email == rhs.email && lhs.recipe == rhs.recipe
+}
+
+
+extension Meals {
+    @discardableResult
+    static func save(_ email: String, recipe: Recipe, copies: Int = 1) -> Meals {
+        let item = Meals()
+        item.createdTime = String(Date().timeIntervalSince1970)
+        
+        item.email = email
+        item.recipe = recipe
+        item.copies = copies
+        
+        var exsit = false
+        for go in User.getAllUsers() {
+            if go == item {
+                exsit = true
+                break
+            }
+        }
+        
+        if !exsit {
+            item.writeToDB()
+        }
+        
+        return item
+    }
+    
+    func update()  {
+        let realm = try! Realm() // Create realm pointing to default file
+        try! realm.write {
+            realm.add(self, update: true)
+        }
+    }
+    
+    func writeToDB() {
+        // Realms are used to group data together
+        let realm = try! Realm() // Create realm pointing to default file
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.add(self)
+        }
+        
+    }
+    
+    func delete() {
+        let realm = try! Realm() // Create realm pointing to default file
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.delete(self)
+        }
+        
+    }
+    
+    
+    static func getMealsBy(_ email: String) -> [Meals] {
+        let realm = try! Realm() // Create realm pointing to default file
+        let results = realm.objects(Meals.self).sorted(byKeyPath: "createdTime", ascending: false).filter("email == '\(email)'")
+        let array = results.get(offset: 0, limit: results.count)
+        
+        
+        return array as! [Meals]
+        
+    }
+
+    
+    /// 获取所有
+    static func getAllMeals() -> [Meals] {
+        let realm = try! Realm() // Create realm pointing to default file
+        let results = realm.objects(Meals.self).sorted(byKeyPath: "createdTime", ascending: false)
+        let array = results.get(offset: 0, limit: results.count)
+        return array as! [Meals]
+    }
+}
+
+extension Results {
+    
+    func get <T:Object> (offset: Int = 0, limit: Int ) -> Array<T> {
+        //create variables
+        var lim = 0 // how much to take
+        var off = 0 // start from
+        var l: Array<T> = Array<T>() // results list
+        
+        //check indexes
+        if off<=offset && offset<self.count - 1 {
+            off = offset
+        }
+        if limit > self.count {
+            lim = self.count
+        } else {
+            lim = limit
+        }
+        
+        //do slicing
+        for i in off..<lim{
+            let dog = self[i] as! T
+            l.append(dog)
+        }
+        
+        //results
+        return l
+    }
+}
---
+++ User.swift
@@ -0,0 +1,171 @@
+//
+//  User.swift
+//  Gfintech
+//
+//  Created by Luochun on 2017/2/20.
+//  Copyright © 2017年 Mantis. All rights reserved.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+let kUserInfo = "loginedUser"
+
+public typealias JSONMap = [String : Any]
+
+public class User: Object {
+    private static let _sharedInstance = User()
+    
+    static func shared() -> User {
+        return _sharedInstance
+    }
+
+    
+    
+    /*用户信息*/
+    @objc dynamic  var email: String?
+    @objc dynamic var name: String?
+    @objc dynamic var password: String?
+    @objc dynamic var diet: String?
+    @objc dynamic var service: Int = 1
+    
+    
+    /// 主键
+    @objc dynamic var createdTime: String = ""
+    
+    override public static func primaryKey() -> String? {
+        return "createdTime"
+    }
+    
+
+    
+    ///  绑定用户信息
+    func setUserInfo(_ userInfo: (String, String, String, String, Int)) {
+
+        self.email = userInfo.0
+        self.name = userInfo.1
+        
+        self.password = userInfo.2
+        self.diet = userInfo.3
+        self.service = userInfo.4
+        let data = ["name": name!, "email": email!, "password": password!, "diet": diet!, "service": String(service)]
+        UserDefaults.standard.set(data, forKey: kUserInfo)
+    }
+    
+    
+    static func logout() {
+        UserDefaults.standard.removeObject(forKey: kUserInfo)
+        
+        User.shared().email = nil
+        User.shared().name = nil
+        User.shared().password = nil
+        User.shared().diet = nil
+        User.shared().service = 1
+        
+    }
+
+    static var isLogined: Bool {
+        if let data = UserDefaults.standard.dictionary(forKey: kUserInfo) as? [String: String] {
+            let user = (data["email"]!, data["name"]!, data["password"]!, data["diet"]!, Int(data["service"]!)!)
+            User.shared().setUserInfo( user )
+            return true
+        }
+
+        return false
+    }
+
+}
+
+
+func == (lhs: User, rhs: User) -> Bool
+{
+    return lhs.name == rhs.name && lhs.email == rhs.email
+}
+
+
+extension User {
+    @discardableResult
+    static func save(_ name: String, email: String, password: String, diet: String, service: Int = 1) -> User {
+        let item = User()
+        item.createdTime = String(Date().timeIntervalSince1970)
+        
+        item.name = name
+        item.email = email
+        item.password = password
+        item.diet = diet
+        item.service = service
+        
+        var exsit = false
+        for go in User.getAllUsers() {
+            if go == item {
+                exsit = true
+                break
+            }
+        }
+        
+        if !exsit {
+            item.writeToDB()
+        }
+        User.shared().setUserInfo( (email, name, password, diet, service) )
+        return item
+    }
+    
+    func update()  {
+        let realm = try! Realm() // Create realm pointing to default file
+        try! realm.write {
+            realm.add(self, update: true)
+        }
+    }
+    
+    func writeToDB() {
+        // Realms are used to group data together
+        let realm = try! Realm() // Create realm pointing to default file
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.add(self)
+        }
+        
+    }
+    
+    func delete() {
+        let realm = try! Realm() // Create realm pointing to default file
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.delete(self)
+        }
+        
+    }
+    
+    
+    static func getUserBy(_ email: String, pwd: String) -> User? {
+        let realm = try! Realm() // Create realm pointing to default file
+        let results = realm.objects(User.self).sorted(byKeyPath: "createdTime", ascending: false).filter("email == '\(email)' and password == '\(pwd)'")
+        let array = results.get(offset: 0, limit: results.count)
+        
+        if array.count > 0 {
+            return array[0] as? User
+        }
+        return nil
+        
+    }
+    
+    /// 获取某一类型的
+    static func getAllUserBy(_ diet: String) -> [User] {
+        let realm = try! Realm() // Create realm pointing to default file
+        let results = realm.objects(User.self).sorted(byKeyPath: "createdTime", ascending: false)
+        let array = results.get(offset: 0, limit: results.count) as! [User]
+        return array.filter{$0.diet == diet}
+        
+    }
+    
+    /// 获取所有
+    static func getAllUsers() -> [User] {
+        let realm = try! Realm() // Create realm pointing to default file
+        let results = realm.objects(User.self).sorted(byKeyPath: "createdTime", ascending: false)
+        let array = results.get(offset: 0, limit: results.count)
+        return array as! [User]
+    }
+}
